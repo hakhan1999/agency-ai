a625ee9c64a835fdfe1f7ed1aa038930f96f4688
hakhan1999 <khanhassaan961@gmail.com>
2025-09-17 12:55:24 +0500

    hero section completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import Header from "./components/Header";
+import Hero from "./components/Hero";
 
 const App = () => {
   const [theme, setTheme] = useState(
@@ -8,6 +9,7 @@ const App = () => {
   return (
     <div className="dark:bg-black relative">
       <Header theme={theme} setTheme={setTheme} />
+      <Hero />
     </div>
   );
 };
