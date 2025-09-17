bf4d0822d32307ca0fe6a5d17d40510887bd13eb
hakhan1999 <khanhassaan961@gmail.com>
2025-09-17 13:02:29 +0500

    trusted by section completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import Header from "./components/Header";
 import Hero from "./components/Hero";
+import TrustedBy from "./components/TrustedBy";
 
 const App = () => {
   const [theme, setTheme] = useState(
@@ -10,6 +11,7 @@ const App = () => {
     <div className="dark:bg-black relative">
       <Header theme={theme} setTheme={setTheme} />
       <Hero />
+      <TrustedBy />
     </div>
   );
 };
